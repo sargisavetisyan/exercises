6477853cfdb306262c3da0b909d384704315d9fe
Sargis Avetisyan <a.sargis.avetisyan@gmail.com>
2022-06-21 00:55:24 +0400

    first commit

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -37,23 +37,3 @@ console.log(fibonacci(n))
 // ==============================================================================================================================
 
 
-// Write a recursive function to find first n Fibonacci numbers. The first two terms are 0 and 1.
-//  All other terms are obtained by adding the preceding two terms. First few elements are
-//  0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
-
-/*
-let n = +prompt('enter number!')
-
-function fibonacci(n) {
-    if (n === 1) {
-        return 0
-    } else if (n === 2) {
-        return 1
-    } else {
-        return fibonacci(n - 2) + fibonacci(n - 1)
-    }
-}
-console.log(fibonacci(n))
-*/
-
-// ==============================================================================================================================
