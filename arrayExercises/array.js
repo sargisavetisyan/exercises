087e74764ac19a02c08b730a8bee4a9adf58027e
Sargis Avetisyan <a.sargis.avetisyan@gmail.com>
2022-06-21 00:42:53 +0400

    second commit

--- arrayExercises/array.js
+++ arrayExercises/array.js
@@ -179,37 +179,110 @@ console.log(sort(arr1))
 // Пример массива : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 // Пример вывода : a (5 раз)
 
+// function duplicate(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length; j++) {                           ??????????????????????????????????????????????????
+// ??????????????????????????
+//         }
+//     }
+// }
 
 
+//==========================================================================================================
+
+// 9. Напишите программу на JavaScript, которая печатает элементы следующего массива.
+// Примечание. Используйте вложенные циклы for.
+// Пример массива: var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// Пример вывода :
+// «строка 0»
+// «1»
+// «2»
+// «1»
+// «24»
+// «строка 1»
 
 
+// var a = [1, 2, 1, 24, [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+
+// function print(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (!Array.isArray(arr[i])) {
+//             console.log(`строка ${i}`)
+//             console.log(arr[i])
+//         } else {
+//             console.log(`строка ${i}`)
+//             for (let j = 0; j < arr[i].length; j++) {
+//                 if (!Array.isArray(arr[i][j])) {
+//                     console.log(arr[i][j])
+//                 }
+//             }
 
 
+//         }
+//     }
+// }
+// print(a)
 
 
+// ==========================================================================================================
 
 
+// Given an array of numbers, Push all the zero’s of a given array to the end of the array.
+// For example, if the given arrays is 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0,
+// it should be changed to 1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0. The order of all other elements should be same.
 
 
+/*
+let array = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0]
+let newArr = []
+let zero = []
+function fun(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            newArr.push(arr[i])
+        } else {
+            zero.push(arr[i])
+        }
+    }
+    return array = newArr.concat(zero)
+}
+console.log(fun(array))
+*/
 
+// ==========================================================================================================
 
+// Given a n x n matrix. The problem is to find all the distinct elements common to all rows of the matrix.
+//  The elements can be printed in any order.
 
+/*
+let arr = [
+    [2, 1, 4, 3],
+    [1, 2, 3, 2],
+    [3, 6, 2, 3],
+    [5, 2, 5, 3]
+]
+let a = []
 
+function commonElement(arr) {
 
+    arr[0].forEach(function (val) {
+        let count = 0
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i].includes(val)) {
+                count++
+            }
+        }
+        if (count === arr.length - 1) {
+            a.push(val)
+        }
+    })
+    return a.join(' ')
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(commonElement(arr))
+*/
 
 
 
--- string/string.js
+++ string/string.js
@@ -2,20 +2,70 @@
 // // Input : aasdddf ksf d
 // // Output : a, s, d, f
 
-const str = 'aasdddf ksf d'
+// const str = 'aasdddf ksf d'
 
-function print(str) {
-    let str1 = ''
+// function print(str) {
+//     let str1 = ''
+//     for (let i = 0; i < str.length; i++) {
+//         for (let j = i + 1; j < str.length; j++) {
+//             if (str[i] === str[j] && str[i] !== ' ') {
+//                 if (!(str1.includes(str[i]))) {
+//                     str1 += str[i]
+//                 }
+//             }
+//         }
+//     }
+//     return str1.split('')
+// }
+
+// console.log(print(str))
+
+// ===============================================================================
+
+// 9. Напишите программу на JavaScript, которая принимает на вход строку и меняет регистр каждого символа.
+//  Например, если вы вводите «Быстрый коричневый лис», вывод должен быть «БЫСТРЫЙ КОРИЧНЕВЫЙ ЛИС»
+
+/*
+let string = 'Fast brown fox'
+let uper = 'ABCDEFJHABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+let str1 = ''
+
+function toUpperCase(str) {
     for (let i = 0; i < str.length; i++) {
-        for (let j = i + 1; j < str.length; j++) {
-            if (str[i] === str[j] && str[i] !== ' ') {
-                if (!(str1.includes(str[i]))) {
-                    str1 += str[i]
-                }
-            }
+        if (uper.includes(str[i])) {
+            str1 += str[i]
+        } else {
+            str1 += str[i].toUpperCase()
         }
     }
-    return str1.split('')
+    return str1
 }
 
-console.log(print(str))
+console.log(toUpperCase(string))
+*/
+
+// ===============================================================================
+
+// Pangram is a sentence containing every letter in the English alphabet.
+// Given a string, find all characters that are missing from the string,
+// i.e., the characters that can make string a Pangram.
+
+/*
+let input = 'The quick brown fox jumps'
+let abc = 'abcdefghijklmnopqrstuvwxyz'
+let output = ''
+
+function pangram(str) {
+    for (let i = 0; i < abc.length; i++) {
+        if (!input.toLowerCase().includes(abc[i])) {
+            output += `${abc[i]}`
+        }
+    }
+    return output
+}
+
+console.log(pangram(input))
+*/
+
+// ===============================================================================
